4a7afd44d9812aa835609a2a959630aeeb03fb43
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-26 09:26:24 -0500

    [+] Service|Page > places: Implementa (muestra lista lugares)

--- src/app/places/places.page.ts
+++ src/app/places/places.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../services/places.service';
+import { Place } from '../services/place.model';
 
 @Component({
   selector: 'app-places',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlacesPage implements OnInit {
 
-  constructor() { }
+  /** Atributes */
+  public places: Place[] = [];
+
+  constructor( private _placesService: PlacesService ) { }
 
   ngOnInit() {
+    this.places = this ._placesService .getAll();
   }
 
 }
