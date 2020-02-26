324c5f1cd61d309ae965b01b88bb7519ff48c131
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-26 15:36:33 -0500

    [+] Page > places: Floating Button (Redirect to 'place-add')

--- src/app/places/places.page.ts
+++ src/app/places/places.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { PlacesService } from '../services/places.service';
-import { Place } from '../services/place.model';
+import { PlacesService } from '../services/places.service';     // Service
+import { Place } from '../services/place.model';                // Model
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-places',
@@ -12,7 +13,10 @@ export class PlacesPage implements OnInit {
   /** Atributes */
   public places: Place[] = [];
 
-  constructor( private _placesService: PlacesService ) { }
+  constructor( 
+    private _placesService: PlacesService,
+    private _router: Router
+  ) { }
 
   /** Método del Ciclo de Vida de Angular
    *  Se activa cuando inicializa el componente por primera vez. */
@@ -26,6 +30,11 @@ export class PlacesPage implements OnInit {
   ionViewWillEnter () {
     console .log( 'ionViewWillEnter' );
     this .places = this.places = this ._placesService .getAll();
+  }
+
+  addNewPlace() {
+    console .log( 'Works!' );
+    this ._router.navigate( [ '/place-new' ] );   // Redirecciona
   }
 
 }
