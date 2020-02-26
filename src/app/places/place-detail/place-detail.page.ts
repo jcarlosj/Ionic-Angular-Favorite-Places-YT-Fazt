fc1fd17b65929442a2dc99331ff1964ade0a15d3
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-26 10:05:42 -0500

    [+] Service > page-detail: Implementa (obtiene lugar x ID)

--- src/app/places/place-detail/place-detail.page.ts
+++ src/app/places/place-detail/place-detail.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';               // Router
+import { PlacesService } from '../../services/places.service';  // Service
+import { Place } from '../../services/place.model';
 
 @Component({
   selector: 'app-place-detail',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor() { }
+  /** Atributes */
+  place: Place;
+
+  constructor( 
+    private _activatedRoute: ActivatedRoute,
+    private _placesService: PlacesService
+  ) { }
 
   ngOnInit() {
+    /** Obtenemos los parametros pasados por la URL */
+    this ._activatedRoute .paramMap .subscribe( param => {
+      const id = param .get( 'id' );
+      /** Obtiene datos del lugar */
+      this .place = this ._placesService .getById( Number( id ) );
+      console .log( 'Data', this .place );
+    });
   }
 
 }
