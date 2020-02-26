55a893345824796bf9127509bf8a1f21ce32c802
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-26 11:08:49 -0500

    [+] Page > page-detail: Muestra detalles del lugar

--- src/app/services/places.service.ts
+++ src/app/services/places.service.ts
@@ -8,9 +8,9 @@ export class PlacesService {
 
   /** Atributes */
   private places: Place[] = [
-    { id: 1, city: 'Ipiales, Nariño', name: 'Santuario de Las Lajas', urlImage: 'https://i.pinimg.com/564x/e0/bf/8d/e0bf8d47beb3008dc7b18005a5250515.jpg', comments: [ 'Comentario 1', 'Comentario 2', 'Comentario 3' ] },
-    { id: 2, city: 'Guatape, Antioquia', name: 'Piedra del Peñol', urlImage: 'https://i.pinimg.com/564x/df/77/ec/df77ecb4d2a19a65db55129594b7aee8.jpg', comments: [ 'Comentario 1', 'Comentario 2', 'Comentario 3' ] },
-    { id: 3, city: 'Salento, Quindio', name: 'Valle del Cocora', urlImage: 'https://i.pinimg.com/564x/7e/29/94/7e29942423f67fa68a43796efd736543.jpg', comments: [ 'Comentario 1', 'Comentario 2', 'Comentario 3' ] }
+    { id: 1, city: 'Ipiales, Nariño', name: 'Santuario de Las Lajas', urlImage: 'https://i.pinimg.com/564x/e0/bf/8d/e0bf8d47beb3008dc7b18005a5250515.jpg', comments: [ 'Que hermoso lugar me gustaría conocerlo', 'Recomendado para turístas y peregrinos' ] },
+    { id: 2, city: 'Guatape, Antioquia', name: 'Piedra del Peñol', urlImage: 'https://i.pinimg.com/564x/df/77/ec/df77ecb4d2a19a65db55129594b7aee8.jpg', comments: [ 'La vista mas hermosa que he visto', 'Es un lindo lugar a 2 horas de Medellin por tierra, lo recomiendo.', 'Es un lugar asombro al que espero poder regresar' ] },
+    { id: 3, city: 'Salento, Quindio', name: 'Valle del Cocora', urlImage: 'https://i.pinimg.com/564x/7e/29/94/7e29942423f67fa68a43796efd736543.jpg', comments: [] }
   ];
 
   constructor() { }
